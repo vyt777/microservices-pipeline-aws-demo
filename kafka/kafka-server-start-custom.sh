02029d379be43a0c342eba7f2209a70f90e0fbec
Denis Ivanov <denforlight@gmail.com>
2024-09-06 13:53:40 -0600

    feat: add Kafka and Zookeper setup

---
+++ kafka/kafka-server-start-custom.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Create Kafka topic
+$KAFKA_HOME/bin/kafka-topics.sh --create --topic create_clients --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1 > $KAFKA_HOME/logs/kafka.log 2>&1 &
+$KAFKA_HOME/bin/kafka-topics.sh --create --topic update_clients --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1 > $KAFKA_HOME/logs/kafka.log 2>&1 &
+
+# Launch Zookeeper
+$KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties > $KAFKA_HOME/logs/zookeeper.log 2>&1 &
+
+# Launch Kafka on the foreground
+$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties > $KAFKA_HOME/logs/kafka.log 2>&1 & 
+
+# Wait for Zookeeper and Kafka to start
+sleep 3
